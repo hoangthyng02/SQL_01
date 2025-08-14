3fb1d1998090cee4cde00b816efa13bfad5b2d14
hoangthyng02 <hoangmt.nguyen02@gmail.com>
2025-08-14 00:16:34 -0400

    Create p1.sql

---
+++ p1.sql
@@ -0,0 +1,25 @@
+ex1: 
+select name
+from city
+where population >120000 and COUNTRYCODE='USA'
+ex2:
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE='JPN'
+ex3: 
+SELECT CITY, STATE
+FROM STATION
+ex4: hackerank-weather-observation-station-6: https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+ex5: hackerank-weather-observation-station-7: https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+ex6: hackerank-weather-observation-station-9: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+ex7: hackerank-name-of-employees: https://www.hackerrank.com/challenges/name-of-employees/problem
+ex8: hackerank-salary-of-employees: https://www.hackerrank.com/challenges/salary-of-employees/problem
+Leetcode
+ex9: leetcode-recyclable-and-low-fat-products: https://leetcode.com/problems/recyclable-and-low-fat-products/?envId=top-sql-50
+ex10: leetcode-find-customer-referee: https://leetcode.com/problems/find-customer-referee/?envId=top-sql-50
+ex11: leetcode-big-countries: https://leetcode.com/problems/big-countries/?envId=top-sql-50
+ex12: leetcode-article-views: https://leetcode.com/problems/article-views-i/?envId=top-sql-50
+Datalemur & StrataScratch
+ex13: datalemur-tesla-unfinished-part: https://datalemur.com/questions/tesla-unfinished-parts
+ex14: datalemur-lyft-driver-wages: https://platform.stratascratch.com/coding/10003-lyft-driver-wages?code_type=1
+ex15: datalemur-find-the-advertising-channel: https://platform.stratascratch.com/coding/10002-find-the-advertising-channel-where-uber-spent-more-than-100k-usd-in-2019?code_type=1
