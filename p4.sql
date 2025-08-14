bd60b46640f17abc7df7c669c28458d6f66c2c83
hoangthyng02 <hoangmt.nguyen02@gmail.com>
2025-08-14 12:40:10 -0400

    Create p4.sql

---
+++ p4.sql
@@ -0,0 +1,9 @@
+ex1: datalemur-laptop-mobile-viewership: https://datalemur.com/questions/laptop-mobile-viewership
+
+ex2: datalemur-triangle-judgement: https://leetcode.com/problems/triangle-judgement/?envType=study-plan-v2&envId=top-sql-50
+
+ex3: datalemur-uncategorized-calls-percentage: https://datalemur.com/questions/uncategorized-calls-percentage
+
+ex4: datalemur-find-customer-referee: https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50
+
+ex5: stratascratch the-number-of-survivors: https://platform.stratascratch.com/coding/9881-make-a-report-showing-the-number-of-survivors-and-non-survivors-by-passenger-class?code_type=1
