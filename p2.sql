1098e3a7146e18a42bf169eade07191ef9b1ea95
hoangthyng02 <hoangmt.nguyen02@gmail.com>
2025-08-14 00:54:45 -0400

    p2.sql

---
+++ p2.sql
@@ -0,0 +1,24 @@
+ex1: hackerrank-weather-observation-station-3: https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true ex2: hackerrank-weather-observation-station-4: https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true ex3: hackerrank-the-blunder: https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true Datalemur ex4: datalemur-alibaba-compressed-mean: https://datalemur.com/questions/alibaba-compressed-mean ex5: datalemur-matching-skills: https://datalemur.com/questions/matching-skills ex6: datalemur-verage-post-hiatus-1: https://datalemur.com/questions/sql-average-post-hiatus-1 ex7: datalemur-cards-issued-difference: https://datalemur.com/questions/cards-issued-difference ex8: datalemur-non-profitable-drugs: https://datalemur.com/questions/non-profitable-drugs Leetcode ex9: leetcode-not-boring-movies: https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50 ex10: leetcode-number-of-unique-subject: https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/?envType=study-plan-v2&envId=top-sql-50 ex11: leetcode-find-followers-count: https://leetcode.com/problems/find-followers-count/?envType=study-plan-v2&envId=top-sql-50 ex12: leetcode-classes-more-than-5-students: https://leetcode.com/problems/classes-more-than-5-students/?envType=study-plan-v2&envId=top-sql-50
+ex1: hackerrank-weather-observation-station-3: https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
+
+ex2: hackerrank-weather-observation-station-4: https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+
+ex3: hackerrank-the-blunder: https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true
+
+ex4: datalemur-alibaba-compressed-mean: https://datalemur.com/questions/alibaba-compressed-mean
+
+ex5: datalemur-matching-skills: https://datalemur.com/questions/matching-skills
+
+ex6: datalemur-verage-post-hiatus-1: https://datalemur.com/questions/sql-average-post-hiatus-1
+
+ex7: datalemur-cards-issued-difference: https://datalemur.com/questions/cards-issued-difference
+
+ex8: datalemur-non-profitable-drugs: https://datalemur.com/questions/non-profitable-drugs
+
+ex9: leetcode-not-boring-movies: https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50
+
+ex10: leetcode-number-of-unique-subject: https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/?envType=study-plan-v2&envId=top-sql-50
+
+ex11: leetcode-find-followers-count: https://leetcode.com/problems/find-followers-count/?envType=study-plan-v2&envId=top-sql-50
+
+ex12: leetcode-classes-more-than-5-students: https://leetcode.com/problems/classes-more-than-5-students/?envType=study-plan-v2&envId=top-sql-50
