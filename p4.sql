7f694a99354989c31f74b65918c4e83a9e454f45
hoangthyng02 <hoangmt.nguyen02@gmail.com>
2025-08-21 11:10:02 -0400

    Update p4.sql

--- p4.sql
+++ p4.sql
@@ -1,9 +1,47 @@
-ex1: datalemur-laptop-mobile-viewership: https://datalemur.com/questions/laptop-mobile-viewership
+ex1: 
+  SELECT
+count (*) filter( where device_type = 'laptop') as laptop_views,
+count (*) filter(where device_type in ('tablet', 'phone') )as mobile_views
+from viewership
 
-ex2: datalemur-triangle-judgement: https://leetcode.com/problems/triangle-judgement/?envType=study-plan-v2&envId=top-sql-50
+ex2: 
+  select x,y,z,
+case
+when x+y >z and x+z >y and z+y >x then 'Yes'
+else 'No'
+end triangle
+from triangle
 
-ex3: datalemur-uncategorized-calls-percentage: https://datalemur.com/questions/uncategorized-calls-percentage
+ex3:
+  WITH uncategorised_callers AS (
+  SELECT COUNT(case_id) AS uncategorised_calls
+  FROM callers
+  WHERE call_category IS NULL
+    OR call_category = 'n/a'
+)
 
-ex4: datalemur-find-customer-referee: https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50
+SELECT 
+  ROUND(100.0 * uncategorised_calls 
+    / (SELECT COUNT(*) FROM callers), 1) AS uncategorised_call_pct
+FROM uncategorised_callers;
 
-ex5: stratascratch the-number-of-survivors: https://platform.stratascratch.com/coding/9881-make-a-report-showing-the-number-of-survivors-and-non-survivors-by-passenger-class?code_type=1
+ex 4
+  SELECT name
+FROM Customer
+WHERE referee_id <> 2 OR referee_id IS NULL
+ex 5
+SELECT survived,
+       sum(CASE
+               WHEN pclass = 1 THEN 1
+               ELSE 0
+           END) AS first_class,
+       sum(CASE
+               WHEN pclass = 2 THEN 1
+               ELSE 0
+           END) AS second_class,
+       sum(CASE
+               WHEN pclass = 3 THEN 1
+               ELSE 0
+           END) AS third_class
+FROM titanic
+GROUP BY survived
